2cae7abbede79b28f512750b3c37096207a5dea2
andrew bumgarner <andrewbumgarnerNC@gmail.com>
2022-01-24 17:17:45 -0800

    set up for see on heroku

--- seeders/seed.js
+++ seeders/seed.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const db = require('../model');
 
-mongoose.connect('mongodb://localhost:27017/workout', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/workout', {
   useNewUrlParser: true,
   useFindAndModify: false,
   useUnifiedTopology: true,
